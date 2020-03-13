522b0df89b6312e59c27f239e72744a01c02fbde
Kc Kai <kirstenckai@gmail.com>
2020-03-13 05:27:36 +0000

    Add assertion tests for breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const rl = require('readline');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + args[0], (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  if (data[0] === undefined || body === []) {
-    console.log("Breed not found")
-  } else {
-    console.log(data[0].description.trim())
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
 
-  }
-});
+  
+    const data = JSON.parse(body);
+    if (data[0] === undefined || data.length === 0) {
+      callback(null);
+    } else {
+      callback(null,data[0].description.trim());
+    }
+  });
+  
+};
+
+
+module.exports = { fetchBreedDescription };
